872c10f24e61c78d3b4452ceaaeb6be2b753acf4
Anton Pavl <antonpavl96@gmail.com>
2022-03-16 15:27:11 +0200

    created base layout for 'Editor'

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
 
-const Editor = () => {
-  return <div>Editor</div>;
+const Editor = (props) => {
+  return (
+    <div className="editor-container">
+      <div className="editor-title">
+        displayName
+        <button>O/C</button>
+      </div>
+      <ControlledEditor />
+    </div>
+  );
 };
 
 export default Editor;
